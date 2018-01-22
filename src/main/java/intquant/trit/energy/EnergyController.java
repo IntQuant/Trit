4993135f363dfd2eb6e8364cf07c09d63e139f2b
IQuant <quant3234@gmail.com>
2018-01-22 12:57:41 +0300

    Added energy accepting functions to EnergyController interface

--- src/main/java/intquant/trit/energy/EnergyController.java
+++ src/main/java/intquant/trit/energy/EnergyController.java
@@ -12,6 +12,17 @@ public interface EnergyController {
 		return Math.max(0, manageSpatial(0));
 	}
 	
+	default long getAcceptableLight() {
+		return Math.max(0, getLightStorage() - manageLight(0));
+	}
+	default long getAcceptableForce() {
+		return Math.max(0, getForceStorage() - manageForce(0));
+	}
+	default long getAcceptableSpatial() {
+		return Math.max(0, getSpatialStorage() - manageSpatial(0));
+	}
+	
+	
 	//Should add value to energy and return result
 	default long manageLight(long value)   {return 0;};
 	default long manageForce(long value)   {return 0;};
@@ -34,6 +45,23 @@ public interface EnergyController {
 	default long provideSpatial(long value) {
 		long targetValue = Math.min(getProvideableSpatial(), value);
 		manageSpatial(-targetValue);
+		return targetValue;
+	}
+	
+	
+	default long acceptLight(long value) {
+		long targetValue = Math.min(getAcceptableLight(), value);
+		manageLight(-targetValue);
+		return targetValue;
+	}
+	default long acceptForce(long value) {
+		long targetValue = Math.min(getAcceptableForce(), value);
+		manageForce(targetValue);
+		return targetValue;
+	}
+	default long acceptSpatial(long value) {
+		long targetValue = Math.min(getAcceptableSpatial(), value);
+		manageSpatial(targetValue);
 		return targetValue;
 	}
 }
