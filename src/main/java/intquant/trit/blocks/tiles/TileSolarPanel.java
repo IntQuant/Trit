1efd1915fb313c6724f362f3f52cb62eef3b0c57
IQuant <quant3234@gmail.com>
2018-01-24 20:30:18 +0300

    Solar panels now work only when they see sunlight

--- src/main/java/intquant/trit/blocks/tiles/TileSolarPanel.java
+++ src/main/java/intquant/trit/blocks/tiles/TileSolarPanel.java
@@ -3,13 +3,21 @@ package intquant.trit.blocks.tiles;
 import net.minecraft.util.ITickable;
 
 public class TileSolarPanel extends TileEnergyController implements ITickable{
+	private boolean isValid = false;
+	private int updates = 0;
+	
 	public TileSolarPanel() {
 		super();
 		this.setMaxLightStorage(1000);
 		this.setDoProvide(true);
 	}
 	public void update() {
-		if (world.isDaytime()) {
+		updates++;
+		if (updates>=50) {
+			updates = 0;
+			isValid = world.canBlockSeeSky(pos);
+		}
+		if (isValid && world.isDaytime() && !world.isRaining()) {
 			this.manageLight(Math.min(this.getMaxLightStorage() - this.lightStorage, 1));
 		}
 	}
